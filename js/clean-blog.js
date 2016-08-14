2914181e0ab29cde07e090726a2db9427b8970bf
r.hanson <r.hanson@bibliotheca.com>
2016-08-14 15:38:12 -0500

    update

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1,14 +1,16 @@
 // Floating label headings for the contact form
-$(function() {
+/*$(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
         $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
     }).on("focus", ".floating-label-form-group", function() {
         $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
+    }).on("blur", ".floating-lab
+    el-form-group", function() {
         $(this).removeClass("floating-label-form-group-with-focus");
     });
 });
-
+*/
+/*
 // Navigation Scripts to Show Header on Scroll-Up
 jQuery(document).ready(function($) {
     var MQL = 1170;
@@ -38,3 +40,4 @@ jQuery(document).ready(function($) {
             });
     }
 });
+*/
