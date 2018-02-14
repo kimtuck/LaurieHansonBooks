a584a8d98d23dce77f3dd0281e93cef05465fcd7
a477mzz <robert.hanson@solutiondesign.com>
2018-02-14 10:45:59 -0600

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from  './home/home.component'
-import { AboutTreasureComponent } from  './about-treasure/about-treasure.component'
+import { HomeComponent } from './home/home.component';
+import { AboutTreasureComponent } from './about-treasure/about-treasure.component';
+import { AboutTheBookComponent } from './about-the-book/about-the-book.component';
+import { AboutLaurieComponent } from './about-laurie/about-laurie.component';
+import { NewsAndEventsComponent } from './news-and-events/news-and-events.component';
+import { CharityComponent } from './charity/charity.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'AboutTreasure', component: AboutTreasureComponent }
-];
+  { path: 'AboutTreasure', component: AboutTreasureComponent },
+  { path: 'AboutTheBook', component: AboutTheBookComponent },
+  { path: 'AboutLaurie', component: AboutLaurieComponent },
+  { path: 'NewsAndEvents', component: NewsAndEventsComponent },
+  { path: 'Charity', component: CharityComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule }     from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { AboutTreasureComponent } from './about-treasure/about-treasure.component';
+import { AboutTheBookComponent } from './about-the-book/about-the-book.component';
+import { AboutLaurieComponent } from './about-laurie/about-laurie.component';
+import { NewsAndEventsComponent } from './news-and-events/news-and-events.component';
+import { CharityComponent } from './charity/charity.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    AboutTreasureComponent
+    AboutTreasureComponent,
+    AboutTheBookComponent,
+    AboutLaurieComponent,
+    NewsAndEventsComponent,
+    CharityComponent
   ],
   imports: [
     BrowserModule,
