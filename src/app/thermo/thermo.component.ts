c21fc6e92ca64183b1fc840adadb69c3ca485eea
Robert Hanson <robert.hanson@solutiondesign.com>
2018-02-15 07:29:22 -0600

    angular

--- src/app/thermo/thermo.component.ts
+++ src/app/thermo/thermo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-thermo',
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./thermo.component.scss']
 })
 export class ThermoComponent implements OnInit {
+  @Input() height: number;
+  @Input() max: number;
+  @Input() value: number;
+  @Input() steps: number;
+  intervals: Array<number>;
 
-  constructor() { }
-
+  constructor() {
+    console.log('height ' + this.height)
+    this.intervals=[];
+  }
   ngOnInit() {
+    console.log('height ' + this.height)
+    this.height = this.height || 200;
+    this.max = this.max || 100;
+    this.steps = this.steps || 4;
+
+    this.valstr = $interpolate(scope.format)({ val: scope.value} );
+    scope.percent = scope.value / scope.max * 100;
+
+    for (var step = 0; step <= scope.steps; step++) {
+      var val = (scope.max / scope.steps) * step;
+      var percent = (val / scope.max)*100;
+      var interval = { percent: percent, label: $interpolate(scope.format)({ val: val })};
+      intervals.push(interval);
+    }
+    scope.intervals = intervals;
+
   }
 
 }
