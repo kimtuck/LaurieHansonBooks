8cad19a20365e0929322c34d37d832e3ba109965
Robert Hanson <robert.hanson@solutiondesign.com>
2021-01-31 08:36:38 -0600

    WIP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home bg-red-10">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
