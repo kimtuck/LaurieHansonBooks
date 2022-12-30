2d7b47ddce4323d6918f0e264135eb1bcbf581b9
Robert Hanson <rhanson1@pattersoncompanies.com>
2022-12-30 07:20:06 -0600

    WIP

--- src/components/GoodGirlKarma.vue
+++ src/components/GoodGirlKarma.vue
@@ -1,21 +1,71 @@
 <template>
     <div>
-        <hero />
-        <good-girl-karma-body />
+        <div>
+            <div class="container flex flex-col items-center mx-auto text-color md:flex-row ">
+                <!-- Left Col -->
+                <div class="justify-center w-full px-12 pt-6 pb-6 align-items: center lg:w-1/2">
+                    <vue-agile
+                        :options="agileOptions"
+                        :class="carouselVisibility"
+                        nav-buttons="false"
+                        speed="2500"
+                        fade
+                        pause-on-hover
+                        autoplay
+                    >
+                        <img class="w-full shadow-xl" src="@assets/carousel/Treasure-Annie.png" />
+                        <img class="w-full shadow-xl" src="@assets/carousel/cropped-book-covertreasure.jpg" />
+                        <img class="w-full shadow-xl" src="@assets/carousel/annie2.png" />
+                        <img class="w-full shadow-xl" src="@assets/carousel/Treasure-ball.png" />
+                    </vue-agile>
+                </div>
+                <!-- Right Col -->
+                <div class="flex flex-col items-center w-full text-center text-indigo-400 lg:w-1/2 lg:py-6">
+                    <div class="text-7xl">Treasure's Gift</div>
+                    <div class="m-4">A funny, thoughful story about a Golden Retriever and her best friend.</div>
+
+                    <purple-dragonfly class="w-3/4 m-2 " />
+
+                    <buy-button />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Hero from './Hero.vue';
-import GoodGirlKarmaBody from './GoodGirlKarmaBody.vue';
+import BuyButton from './BuyButton.vue';
+import { VueAgile } from 'vue-agile';
 
 export default defineComponent({
-    name: 'TreasuresGift',
+    name: 'GoodGirlKarma',
     components: {
-        Hero,
-        GoodGirlKarmaBody
+        VueAgile,
+        BuyButton
     },
-    props: {}
+    props: {},
+    data() {
+        return {
+            carousel: ['cropped-book-cover.jpg', 'Annie2.png', 'Treasure-Annie.png', 'Treasure-Ball.png'],
+            carouselVisibility: 'invisible',
+            coverVisibility: '',
+            agileOptions: { navButtons: false }
+        };
+    },
+    mounted() {
+        const self = this;
+        setTimeout(() => {
+            self.carouselVisibility = '';
+            self.coverVisibility = 'hidden';
+        }, 0);
+    },
+    methods: {
+        getImgUrl(pic: string) {
+            // eslint-disable-next=line
+            // eslint-disable-next-line global-require
+            return require(`@assets/carousel/${pic}`);
+        }
+
 });
 </script>
